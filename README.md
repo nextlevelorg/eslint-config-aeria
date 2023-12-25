dc2594e7d7af15c24698f48ed36c847b90fc4851
ringeringeraja <joaosan177@gmail.com>
2023-12-25 02:40:52 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# eslint-config-aeria
+
+Official [shareable ESLint configuration](https://eslint.org/docs/latest/extend/shareable-configs) for Aeria Framework.
---
+++ index.js
@@ -0,0 +1,55 @@
+const stylistic = require('@stylistic/eslint-plugin')
+const parserTs = require('@typescript-eslint/parser')
+
+module.exports = {
+  plugins: {
+    '@stylistic': stylistic,
+  },
+  languageOptions: {
+    parser: parserTs,
+  },
+  files: [
+    '**/*.js',
+    '**/*.ts',
+  ],
+  ignores: [
+    '**/dist/**',
+    '**/*.d.ts',
+  ],
+  rules: {
+    '@stylistic/indent': [
+      'error',
+      2,
+    ],
+    '@stylistic/comma-dangle': [
+      'error',
+      'always-multiline',
+    ],
+    '@stylistic/quotes': [
+      'error',
+      'single',
+    ],
+    'no-var': 'error',
+    'prefer-const': [
+      'error',
+      {
+        destructuring: 'all',
+      },
+    ],
+    '@stylistic/array-element-newline': [
+      'error',
+      {
+        minItems: 2,
+      },
+    ],
+    '@stylistic/array-bracket-newline': [
+      'error',
+      {
+        minItems: 2,
+      },
+    ],
+    '@stylistic/object-property-newline': 'error',
+    '@stylistic/object-curly-newline': 'error',
+    'eqeqeq': 'error',
+  },
+}
